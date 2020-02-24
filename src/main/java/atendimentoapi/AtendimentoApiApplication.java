45c2e7cecb3d95a0233879b5e83f88000f641c4a
Gustavo Avila <gustavogavila01@gmail.com>
2020-02-24 13:11:32 -0300

    Início do projeto

---
+++ src/main/java/atendimentoapi/AtendimentoApiApplication.java
@@ -0,0 +1,13 @@
+package atendimentoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtendimentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtendimentoApiApplication.class, args);
+	}
+
+}
